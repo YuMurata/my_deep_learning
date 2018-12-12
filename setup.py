e77cd1151ead1f341d3e87450bfc697bdf1daa9b
purin52002 <purin7635@gmail.com>
2018-12-12 14:38:44 +0900

    add python project template

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup, find_packages
 
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
 setup(
     name='my_deep_learning',
     version='0.0.1',
     description='deep learning utility',
+    long_description=readme,
     author='Yu Murata',
     author_email='me@kennethreitz.com',
     url='https://github.com/kennethreitz/samplemod',
+    license=license,
     packages=find_packages(exclude=('tests', 'docs')),
-    install_requires=['numpy'],
+    install_requires=[
+        'numpy',
+        'tensorflow'
+        ],
 )
